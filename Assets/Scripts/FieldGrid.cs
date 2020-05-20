681a37dcb8be6018648446edd2aa05e465c1cdf0
Aleksandr <shumaherr@gmail.com>
2020-05-21 01:56:56 +0200

    Init commit

---
+++ Assets/Scripts/FieldGrid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldGrid
+{
+    private int width, height;
+    private float cellSize = 10f;
+    private GameObject[,] field;
+
+    public FieldGrid(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+        field = new GameObject[height, width];
+    }
+
+    public void PlaceUnit(int x, int y, GameObject unit)
+    {
+        field[x, y] = unit;
+    }
+
+    public GameObject GetUnit(int x, int y)
+    {
+        return field[x, y] != null ? field[x, y] : null;
+    }
+
+}
